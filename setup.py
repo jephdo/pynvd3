e5175894d49afe8205f0f969ffc4ea9eecec0f72
Jeph Do <jdo@solarcity.com>
2014-02-26 23:16:51 -0800

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='pynvd3',
+    version='0.01',
+    description='A Python wrapper for NVD3.js',
+    url='http://github.com/jephdo/pynvd3/',
+    author='Jeph Do',
+    author_email='jephdo@gmail.com',
+    packages=[
+        'pynvd3', 
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
